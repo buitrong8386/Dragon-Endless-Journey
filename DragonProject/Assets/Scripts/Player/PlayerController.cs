bea177cd0799ac6a67cc5057cd7b33d1c86839ea
Hoang-DucViet <hoangducviet1912201@gmail.com>
2025-02-27 09:37:36 +0700

    Xong core chinh

--- DragonProject/Assets/Scripts/Player/PlayerController.cs
+++ DragonProject/Assets/Scripts/Player/PlayerController.cs
@@ -5,29 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Player player;
+    private PanelManager panelManagerScript;
     void Start()
     {
+        panelManagerScript = GameObject.Find("Canvas").GetComponent<PanelManager>();
         player = new Player
         {
             Name = "Player",
             Health = 100,
         };
     }
+    void Update()
+    {
+        CheckPosition();
+    }
 
     public void TakeDamage(int damage)
     {
         player.Health -= damage;
+        Debug.Log(this.transform.position.y);
         if (player.Health <= 0)
         {
-            Destroy(gameObject);
+            Death();
         }
     }
-     public void OnCollisionEnter2D(Collision2D collision)
+
+    void CheckPosition()
+    {
+        if (this.transform.position.y < -5.5f)
+        {
+            Death();
+        }
+    }
+        public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == TagContants.TRAP)
         {
             Destroy(collision.gameObject.GetComponent<BoxCollider2D>());
           
         }
+    }
+
+    
+    void Death()
+    {
+        Destroy(gameObject);
+        panelManagerScript.GameOver();
     }
 }
